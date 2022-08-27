fc1c37a5f4103266b7f07d475836af3e21ad862b
hakkicansengonul <coder.9734@gmail.com>
2022-08-27 12:19:56 +0300

    Initial Commit

---
+++ Tinder-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinder-Swift
+//
+//  Created by hakkı can şengönül on 27.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
